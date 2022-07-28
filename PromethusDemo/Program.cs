6692ad34caeebddd004ca667fec20f6b1cbfff8d
Parikshanth <parikshanth@hotmail.com>
2022-07-28 09:52:59 +0530

    Add project files.

---
+++ PromethusDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Prometheus;
+using Prometheus.SystemMetrics;
+
+namespace PromethusDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .Enrich.FromLogContext()
+                .WriteTo.Console(outputTemplate: "{Timestamp:G} [{Level:u3}] [{SourceContext}] {Message} {NewLine:1}{Exception:1}")
+                .CreateLogger();
+
+            var server = new MetricServer(hostname: "localhost", port: 9090);
+            
+            server.Start();
+
+            await new HostBuilder().ConfigureServices((hostContext, service) =>
+                {
+                    service.AddSystemMetrics();
+                    
+                    service.AddScoped<IHostedService, SampleService>();
+                })
+                .UseSerilog()
+                .RunConsoleAsync();
+        }
+    }
+}
---
+++ PromethusDemo/SampleService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Prometheus;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PromethusDemo
+{
+    internal class SampleService : IHostedService
+    {
+        private readonly ILogger<SampleService> _logger;
+        CancellationTokenSource cts;
+
+        private readonly Counter counter = Metrics.CreateCounter("sampleapp_ticks_total", "running counter");
+
+
+        public SampleService(ILogger<SampleService> logger)
+        {
+            _logger = logger;
+            cts = new CancellationTokenSource();
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    _logger.LogInformation("Running");
+                    counter.Inc();
+                    Thread.Sleep(1000);
+                }
+            }, cts.Token);
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            cts.Cancel();
+            _logger.LogWarning("Stopping");
+            return Task.CompletedTask;
+        }
+    }
+}
